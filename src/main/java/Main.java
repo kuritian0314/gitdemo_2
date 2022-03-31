aac15c597aaf31d1924a39e8a2210bc345a7684d
weihua <1318022435@qq.com>
2022-04-01 01:27:37 +0800

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author curry
+ * @package PACKAGE_NAME
+ * @date 2022/4/1 1:25
+ * @Version V1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
